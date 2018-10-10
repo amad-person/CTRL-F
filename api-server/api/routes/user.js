85505111beb1a6b5be88dd08e882ef71a6aeec7c
Sashankh Chengavalli Kumar <cksashankh@gmail.com>
2018-10-11 00:39:58 +0800

    Allow single file request only

--- api-server/api/routes/user.js
+++ api-server/api/routes/user.js
@@ -15,16 +15,24 @@ router.get('/', function (req, res) {
 
 router.post('/upload', function (req, res) {
     console.log('Request received at /upload');
-    // console.log('Request: ', req.headers);
-    console.log('req.files = ', req.files);
-    var fName = undefined;
-    if (req.files) {
+    if (req.files && req.files.length === 1) {
         req.files.forEach(element => {
             fs.renameSync('./uploads/' + element.filename, './uploads/' + element.originalname, err => {
                 console.log(err);
             });
         });
         res.end('File(s) uploaded');
+    } else if (!req.files || req.files.length === 0) {
+        res.statusCode = 404;
+        res.end('File not received');
+    } else if (req.files.length > 1) {
+        req.files.forEach(element => {
+            fs.unlinkSync(element.path, err => {
+                console.log(err);
+            });
+        });
+        res.statusCode = 400;
+        res.end('Only one file per request');
     }
 });
 
