7988d7c9839bc2c82235a498372bfb397e613e1e
amad-person <aadyaa@u.nus.edu>
2018-10-11 13:46:31 +0800

    Pass file to parent, UI tweaks

--- ctrl-f-client/src/containers/App/App.js
+++ ctrl-f-client/src/containers/App/App.js
@@ -1,17 +1,37 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Header from '../../components/Header/Header';
 import FileDrop from '../FileDrop/FileDrop';
+import { Button } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header/>
-        <FileDrop/>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            doneUploading: false,
+            files: []
+        };
+    }
+
+    dataFromFileDrop = (fileData) => {
+        this.setState({
+            doneUploading: fileData.doneUploading,
+            files: fileData.files
+        });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+                <FileDrop dataFromFileDrop={this.dataFromFileDrop}/>
+                { this.state.doneUploading ?
+                    <Button color="success" size="lg">Process</Button> :
+                    'Upload a video!'
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
--- ctrl-f-client/src/containers/FileDrop/FileDrop.js
+++ ctrl-f-client/src/containers/FileDrop/FileDrop.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import ReactDropzone from 'react-dropzone';
+import Dropzone from 'react-dropzone';
 import request from 'superagent';
 import './FileDrop.css'
 
 class FileDrop extends Component {
     constructor(props) {
         super(props);
-        this.state = { files: [] }
+        this.state = { files: [] };
     }
 
+    // accept file and send to server
     onDrop = (files) => {
         this.setState({
             files
@@ -24,12 +25,31 @@ class FileDrop extends Component {
         req.attach(files[0].name, files[0]);
 
         req.end();
+
+        // send data to parent component
+        const fileData = {
+            files: this.state.files,
+            doneUploading: true
+        };
+
+        this.props.dataFromFileDrop(fileData);
+    };
+
+    getFileSize = (size) => {
+        let sizeExt = ['bytes', 'KB', 'MB', 'GB'], i = 0;
+
+        while (size > 900) {
+            size /= 1000;
+            i++;
+        }
+
+        return '' + (Math.round(size * 100) / 100) + ' ' + sizeExt[i];
     };
 
     render() {
         return (
             <div className="FileDrop">
-                <ReactDropzone
+                <Dropzone
                     accept="video/mp4"
                     className="Dropzone"
                     activeClassName="DropzoneActive"
@@ -39,7 +59,7 @@ class FileDrop extends Component {
                 >
                     {({ isDragAccept, isDragReject, acceptedFiles, rejectedFiles }) => {
                         if (acceptedFiles.length || rejectedFiles.length) {
-                            return `Accepted file, please wait for CTRL-F!`;
+                            return `Uploaded file, now click on process!`;
                         }
                         if (isDragAccept) {
                             return "You can upload this file!";
@@ -49,8 +69,8 @@ class FileDrop extends Component {
                         }
                         return "Drop a video file, or click to browse.";
                     }}
-                </ReactDropzone>
-                <div className="DroppedFileInfo">{this.state.files.map(f => <p key={f.name}>{f.name} - {f.size} bytes</p>)}</div>
+                </Dropzone>
+                <div className="DroppedFileInfo">{this.state.files.map(f => <p key={f.name}>{f.name} ~ {this.getFileSize(f.size)}</p>)}</div>
             </div>
         );
     };
